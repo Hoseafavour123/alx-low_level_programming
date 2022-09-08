fa96de8e01d6b0ffcf4677c83e2b177e8d12312f
Hosea Tonzai Favour <hoseafavour123@gmail.com>
2022-09-08 01:48:10 -0700

    Use printf to display strings

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Description - A program that prints "with proper grammer,
+  * but the outcome is a piece of art"
+  *
+  * Return: 0
+  */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
+
