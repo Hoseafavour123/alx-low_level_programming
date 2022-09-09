f93da5f792b0ca988fd4388b42a00b57b48fc550
Hosea Tonzai Favour <hoseafavour123@gmail.com>
2022-09-09 01:07:43 -0700

    Determine wether random number is positive, negative or zero

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description - A program to determine
+ * wether a randomly generated number
+ * is positive or negative.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+
+
+	return (0);
+}
+
