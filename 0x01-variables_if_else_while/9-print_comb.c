990a1bf80125811770e7ea5ea5204814881589de
Hosea Tonzai Favour <hoseafavour123@gmail.com>
2022-09-09 10:46:58 -0700

    Print digits seperated by commas

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  * Description - A program that prints all single digits of base 10
+  * seperated by commmas
+  *
+  * Return: Always 0 (Success)
+  */
+
+int main(void)
+{
+	int max;
+	int min;
+
+	max = 58;
+	min = 48;
+
+
+	while (min < max)
+	{
+
+		putchar(min);
+		
+		if (min != 57)
+		{
+			putchar(',');
+			putchar(' ');
+	
+		}
+
+		min++;
+
+	}
+
+	putchar(10);
+
+
+	return (0);
+}
