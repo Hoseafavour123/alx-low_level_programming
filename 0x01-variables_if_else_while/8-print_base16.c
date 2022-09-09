038e47267a2f4d7aed82c7db4f2e7b8793d2060c
Hosea Tonzai Favour <hoseafavour123@gmail.com>
2022-09-09 10:08:34 -0700

    Print base 16 numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  * Description - A program that prints all single digits of base 10
+  *
+  * Return: Always 0 (Success)
+  */
+
+int main(void)
+{
+	int max;
+	int min;
+	int a;
+
+	max = 57;
+	min = 48;
+	a = 97;
+
+
+	while (min <= max)
+	{
+
+		putchar(min);
+		min++;
+
+	}
+
+
+	while  (a <= 101)
+	{
+		putchar(a);
+		a++;
+	}
+
+	putchar(10);
+
+
+	return (0);
+}
