8971efbbc410cf7506a298a055fbe5e10a1aaede
Hosea Tonzai Favour <hoseafavour123@gmail.com>
2022-09-12 07:28:42 -0700

    Print two two-digits combos

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Description - A program that prints all possible combinations
+  * of two two-digit numbers
+  *
+  * Return: Always 0 (Success)
+  */
+
+int main(void)
+{
+	int a, b, c, d;
+
+	a = 48;
+	while (a < 58)
+	{
+		b = 48;
+
+		while (b < 58)
+		{
+			c = a;
+			d = b + 1;
+
+			while (c < 58)
+			{
+				while (d < 58)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(32);
+					putchar(c);
+					putchar(d);
+
+					if ((a < 57) || (b < 56) || (c < 57) || (d < 57))
+					{
+						putchar(',');
+						putchar(32);
+
+					}
+					d++;
+				}
+				c++;
+				d = 0;
+			}
+			b++;
+		}
+		a++;
+	}
+
+	putchar(10);
+	return (0);
+}
+
