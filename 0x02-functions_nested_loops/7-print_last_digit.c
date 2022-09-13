ae77b2921108a003161bd05ee23faae434f8e37e
Hosea Tonzai Favour <hoseafavour123@gmail.com>
2022-09-13 05:40:45 -0700

    Print last digit of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * print_last_digit - compute the last digit of an argument
+  * @n: argument
+  *
+  * Return: last digit of n
+  */
+
+int print_last_digit(int n)
+{
+	int last_digit = (n % 10);
+
+	return (last_digit);
+}
+
