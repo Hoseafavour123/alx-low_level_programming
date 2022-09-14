5046c67804c3cd8503a5732501b20736d53aa7fc
Hosea Tonzai Favour <hoseafavour123@gmail.com>
2022-09-14 14:03:54 -0700

    Persisting

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -33,15 +33,15 @@ void print_times_table(int n)
 			else if ((prod > 9) && (prod <= 99))
 			{
 				_putchar(' ');
-				_putchar(prod / 10 + '0');
-				_putchar(prod % 10 + '0');
+				_putchar((prod / 10) + '0');
+				_putchar((prod % 10) + '0');
 			}
 
 			else if (prod >= 100)
 			{
-				_putchar((prod / 100) % 10 + '0');
-				_putchar((prod / 10) % 10 + '0');
-				_putchar(prod % 10);
+				_putchar(((prod / 100) % 10) + '0');
+				_putchar(((prod / 10) % 10) + '0');
+				_putchar((prod % 10) + '0');
 
 			}
 		}
