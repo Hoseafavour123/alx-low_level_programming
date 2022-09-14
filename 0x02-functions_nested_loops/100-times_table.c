bf2b51b517f1a4e44f9770cc438098439fcf8e5f
Hosea Tonzai Favour <hoseafavour123@gmail.com>
2022-09-14 13:07:28 -0700

    Print times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,5 +4,48 @@
   * print_times_table - displays times table
   * @n: argument
   *
-  *
+  * Return : void
+  */
+
+void print_times_table(int n)
+{
+	int i, j, prod;
+
+	for (i = 0; i <= n; i++)
+	{
+		if ((n < 0) || (n > 15))
+			break;
+		_putchar('0');
+		
+		for (j = 1; j <= n; j++)
+		{
+			prod = i * j;
+			_putchar(',');
+			_putchar(' ');
+
+			if (prod <= 9)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(prod);
+			}
+
+			else if ((prod > 9) && (prod <= 99))
+			{
+				_putchar(' ');
+				_putchar(prod / 10 + '0');
+				_putchar(prod % 10 + '0');
+			}
+
+			else if (prod >= 100)
+			{
+				_putchar((prod / 100) % 10 + '0');
+				_putchar((prod / 10) % 10 + '0');
+				_putchar(prod % 10);
+
+			}
+		}
+		_putchar('\n');
+	}
+}
 
