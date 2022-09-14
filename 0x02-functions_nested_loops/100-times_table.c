8719b1b067df2832976eeb9fa9ea7340e4a7be90
Hosea Tonzai Favour <hoseafavour123@gmail.com>
2022-09-14 08:31:09 -0700

    Debugging

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,52 +1,8 @@
 #include "main.h"
 
 /**
- * print_times_table - print the times table of argument
- * @n: argument
- *
- * Return: void
- */
+  * print_times_table - displays times table
+  * @n: argument
+  *
+  *
 
-void print_times_table(int n)
-{
-	int i, j, prod, ldgt;
-
-	for (i = 0; i <= n; i++)
-	{
-		if ((n > 15) || (n < 0))
-			break;
-		_putchar('0');
-		_putchar(',');
-		_putchar(' ');
-		for (j = 1; j <= n; j++)
-		{
-			prod = (i * j);
-			if (prod <= 9)
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(prod + '0');
-			}
-			else if ((prod > 9) && (prod <= 99))
-			{
-				_putchar(' ');
-				_putchar((prod / 10) + '0');
-				_putchar((prod % 10) + '0');
-				_putchar(',');
-			}
-			else if (prod >= 100)
-			{
-				ldgt = prod % 10;
-				prod = prod / 10;
-				if (prod >= 10)
-				{
-					_putchar((prod / 10) + '0');
-					_putchar((prod % 10) + '0');
-					_putchar(ldgt);
-					_putchar(',');
-				}
-			}
-		}
-		_putchar('\n');
-	}
-}
