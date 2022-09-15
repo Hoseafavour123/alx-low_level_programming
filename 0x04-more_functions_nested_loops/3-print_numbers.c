536fa2f6bf6ba1f4d18b4a67904be402ae0f573c
Hosea Tonzai Favour <hoseafavour123@gmail.com>
2022-09-15 01:19:41 -0700

    Print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * print_numbers - print numbers 0-9
+  *
+  * Return: void
+  */
+
+void print_numbers(void)
+{
+	int i;
+	int j;
+
+	i = 48;
+	j = 57;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar(10);
+}
+
