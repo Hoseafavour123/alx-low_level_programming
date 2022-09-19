64639286c57b6b98f8ae75651f34e5651f7de3c9
Hosea Tonzai Favour <hoseafavour123@gmail.com>
2022-09-19 09:37:52 -0700

    Print reverse

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-  * rev_string - reverses a string
+  * rev_string - reverse string
   * @s: argument
   *
   * Return: 0
@@ -9,28 +9,14 @@
 
 void rev_string(char *s)
 {
-	int i, len, j, k;
+	int i = 0, len = 0;
 	char tmp;
 
-	i = 0;
-
-	while (s[i])
+	while (i = len - 1; i >= len / 2; i--)
 	{
-		len += 1;
+		tmp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = tmp;
 	}
-
-	j  = len - 1;
-	k = 0;
-
-	while (k < j)
-	{
-		tmp = s[k];
-		s[k] = s[j];
-		s[j] = tmp;
-		k++;
-		j--;
-	}
-
-	_putchar(10);
 }
 
