7258b7ecd96d184fd5317f4c63dc95f27382b011
Hosea Tonzai Favour <hoseafavour123@gmail.com>
2022-09-19 05:57:27 -0700

    Print string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+  * print_rev - reverses a string
+  * @s: argument
+  *
+  * Return: void
+  */
+
+void print_rev(char *s)
+{
+	int i, j, len;
+
+	i = 0;
+	len = 0;
+
+	while (s[i])
+	{
+		len += 1;
+		i++;
+	}
+
+	for (j = len; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+
+	_putchar(10);
+}
+
