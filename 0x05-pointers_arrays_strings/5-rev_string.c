83d980de4ae59095943b6353b84c8d7801397c45
Hosea Tonzai Favour <hoseafavour123@gmail.com>
2022-09-19 07:36:48 -0700

    Reverese

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,30 +9,30 @@
 
 void rev_string(char *s)
 {
-	int i, k, j, len;
+	int i, j, len, end;
+	char rev[1000];
 
 	i = 0;
-	k = 0;
 	len = 0;
-
-	while (s[k])
-	{
-		_putchar(s[k]);
-		k++;
-	}
 
 	while (s[i])
 	{
+		_putchar(s[i]);
 		len += 1;
 		i++;
 	}
 
+	end = len - 1;
+
+	for (j = 0; j <= end; j++)
+	{
+		rev[j] = s[end];
+		_putchar(rev[j]);
+		end--;
+	}
+
 	_putchar(10);
 
-	for (j = len - 1; j >= 0; j--)
-	{
-		_putchar(s[j]);
-	}
 
 	_putchar(10);
 }
