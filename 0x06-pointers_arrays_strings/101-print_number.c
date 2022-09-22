65bdb367faa99ae9a657b8dcdcf9880724ff24fa
Hosea Tonzai Favour <hoseafavour123@gmail.com>
2022-09-22 06:04:48 -0700

    Print passed integers to console

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  * print_number - prints the number passed to it
+  * @n: argument
+  *
+  * Return: void
+  */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = n * -1;
+	}
+
+	if (n / 10 != 0)
+	{
+		print_number(n / 10);
+	}
+
+	_putchar((n % 10) + '0');
+}
+
