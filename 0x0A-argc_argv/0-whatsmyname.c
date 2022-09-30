20d1e83a51451aa9ebd575f6bc84f8b0aeefad2b
Hosea Tonzai Favour <hoseafavour123@gmail.com>
2022-09-30 01:08:55 -0700

    Command line arguments

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+  * main - prints the name of the program
+  * @argc: argument count
+  * @argv: argument value
+  *
+  * Return: 0
+  */
+
+int main(int argc __attribute__((unused)), char **argv)
+{
+	printf("%s\n", *(argv));
+
+	return (0);
+}
+
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+Tasks on Command Line Arguments
