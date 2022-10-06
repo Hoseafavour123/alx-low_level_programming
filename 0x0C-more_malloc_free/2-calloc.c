1cf938b8cf381075d409d7062c810ec3666c5ab4
Hosea Tonzai Favour <hoseafavour123@gmail.com>
2022-10-06 16:14:50 -0700

    Creating calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,23 +2,23 @@
 #include "main.h"
 
 /**
-  * _calloc - allocates memory for an array
-  * @nmemb: number of blocks
-  * @size: size in bytes of each blocks
-  *
-  * Return: pointer to allocated blocks
-  */
+ * _calloc - allocates memory for an array
+ * @nmemb: number of blocks
+ * @size: size in bytes of each blocks
+ *
+ * Return: pointer to allocated blocks
+ */
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int i, j;
-	void *alloc_mem;
+	unsigned int i;
+	char *alloc_mem;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
 	alloc_mem = (void *) malloc(size * nmemb);
-	
+
 	if (alloc_mem == NULL)
 		return (NULL);
 
