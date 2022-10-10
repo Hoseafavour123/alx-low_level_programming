77f3f3881d83ae7f0bf4a53bd8a37c2625c4de3e
Hosea Tonzai Favour <hoseafavour123@gmail.com>
2022-10-10 09:00:50 -0700

    Printing structures

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,21 +10,21 @@
 
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
-		return;
-	if (!(d->name))
-		printf("Name: (nil)\n");
-	else
-		printf("Name: %s\n", d->name);
+	if (d != NULL)
+	{
+		if (d->name == NULL)
+			printf("(nill)\n");
+		else
+			printf("Name: %s\n", d->name);
 
-	if (!(d->age))
-		printf("Age: (nil)\n");
-	else
-		printf("Age: %f\n", d->age);
+		if (!(d->age))
+			printf("(nill)\n");
+		else
+			printf("%f\n", d->age);
 
-	if (!(d->owner))
-		printf("Owner: (nil)\n");
-	else
-		printf("Owner: %s\n", d->owner);
+		if (d->owner == NULL)
+			printf("(nill)\n");
+		else
+			printf("%s\n", d->owner);
+	}
 }
-
