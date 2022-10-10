030d0bf794dd22700fb0ecf436500a4b2c9f4387
Hosea Tonzai Favour <hoseafavour123@gmail.com>
2022-10-10 09:09:47 -0700

    Tracking bugs

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,17 +13,17 @@ void print_dog(struct dog *d)
 	if (d != NULL)
 	{
 		if (d->name == NULL)
-			printf("(nill)\n");
+			printf("Name: (nil)\n");
 		else
 			printf("Name: %s\n", d->name);
 
-		if (!(d->age))
-			printf("(nill)\n");
+		if (d->age < 0)
+			printf("Age: (nil)\n");
 		else
 			printf("%f\n", d->age);
 
 		if (d->owner == NULL)
-			printf("(nill)\n");
+			printf("(nil)\n");
 		else
 			printf("%s\n", d->owner);
 	}
