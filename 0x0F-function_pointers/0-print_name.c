d9e2728f2d152e9d2cc93324d783724eb75d079a
Hosea Tonzai Favour <hoseafavour123@gmail.com>
2022-10-11 23:21:01 -0700

    Printing name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * print_name - prints the name passed to it
+  * @name: name
+  * @(*f)(char *): function pointer that points to the function
+  * to work on
+  *
+  * Return: void
+  */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || (*f) == NULL)
+		return;
+
+	(*f)(name);
+}
+
