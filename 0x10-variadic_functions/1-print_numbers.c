61863431dec0553998302453b78fb96ea621c33d
Hosea Tonzai Favour <hoseafavour123@gmail.com>
2022-10-13 09:22:39 -0700

    Printing numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -24,12 +24,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		num = va_arg(ptr, int);
 		printf("%d", num);
 
-		if (separator == NULL)
-		{
-			;
-		}
-
-		if (i != (n - 1))
+		if (i != (n - 1) && separator != NULL)
 		{
 			printf("%s", separator);
 		}
