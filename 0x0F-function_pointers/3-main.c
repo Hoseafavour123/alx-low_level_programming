95139970760e2fc8f7aa6e46c89767c6254c65a5
Hosea Tonzai Favour <hoseafavour123@gmail.com>
2022-10-13 02:38:18 -0700

    Function pointing

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -19,22 +19,20 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		exit(98);
 	}
-	if ((argv[2][0] == '/' || argv[2][0] == '%') && (argv[3][0] == 0))
-	{
-		printf("Error\n");
-		exit(100);
-	}
+
 	if (argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
+
 	funcptr = get_op_func(argv[2]);
 	if (funcptr == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
+
 	printf("%d\n", funcptr(atoi(argv[1]), atoi(argv[3])));
 
 	return (0);
