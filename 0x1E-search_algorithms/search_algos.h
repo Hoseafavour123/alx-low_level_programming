d5afbaaa5d69ac22b7cf31cb96eb99d114f8ca43
Hosea Tonzai Favour <hoseafavour123@gmail.com>
2023-05-08 23:13:31 +0000

    Added headers

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,6 +4,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+  * struct skiplist_s - a structure
+  * @n: number
+  * @index: index
+  * @next: next pointer
+  * @express: express
+  */
+typedef struct skiplist_s
+{
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
+} skiplist_t;
+
+
+/**
+  * struct listint_s - singly linked list
+  * @n: integer
+  * @index: index
+  * @next: pointer to next int
+  */
+typedef struct listint_s
+{
+	int n;
+	size_t index;
+	struct listint_s *next;
+} listint_t;
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
@@ -11,4 +39,6 @@ int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
+skiplist_t *linear_skip(skiplist_t *list, int value);
 #endif
